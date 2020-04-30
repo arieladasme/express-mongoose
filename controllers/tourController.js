4492bdbea784f9c2b6b4bf65f3aa91bfd6b79bee
Ariel <ariel.wm1@gmail.com>
2020-04-30 16:34:38 -0400

    mongoose-models-schemas

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,19 +1,8 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -29,10 +18,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
+    /* results: tours.length,
     data: {
       tours,
-    },
+    }, */
   });
 };
 
@@ -50,25 +39,12 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  //console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    /* data: {
+      tour: newTour,
+    }, */
+  });
 };
 
 exports.updateTour = (req, res) => {
