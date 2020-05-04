8b5edd4fa388f424d383a048ea0dce2d27b74588
Ariel <ariel.wm1@gmail.com>
2020-05-04 14:07:17 -0400

    Aggregation Pipeline: match, group

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getToursStats,
 } = require('../controllers/tourController');
 
 const router = express.Router();
@@ -14,6 +15,8 @@ const router = express.Router();
 // router.param('id', checkID);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getToursStats);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
