2499def27c47ef52de8ac7478adea6c769d7487e
Ariel <ariel.wm1@gmail.com>
2020-05-11 19:21:16 -0400

    Mongoose Middleware Document

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -64,6 +66,28 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// Document Middleware: se ejecuta antes de comando save y el create
+// con esto tenemos acceso al documento que se esta procesando(creando)
+// this: es el documento procesado actualmente
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+/* 
+// pre save middlewar
+tourSchema.pre('save', function (next) {
+  console.log('Will save document');
+  next();
+});
+
+// Middleware despues de documento guardado
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
+}); 
+*/
 
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
