3a66149f6df5745ea0185d21b423a57ed6ab15cc
Ariel <ariel.wm1@gmail.com>
2020-05-19 19:29:01 -0400

    Implementacion middeware global para errores

--- app.js
+++ app.js
@@ -34,9 +34,30 @@ app.use('/api/v1/users', userRouter)
 // all: aplica esto a todos los metodos
 // req.originalUrl: url que agrego el usuario
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // })
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`)
+  err.status = 'fail'
+  err.statusCode = 404
+
+  next(err)
+})
+
+/*
+ * MIDDLEWARE GLOBAL: Manejo de errores
+ * status(500): error internal server
+ * status(400): status fail
+ */
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   })
 })
 
