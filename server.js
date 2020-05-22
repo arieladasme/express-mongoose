8fb4b7f3b9951349c53899411464716bf3d9047e
Ariel <ariel.wm1@gmail.com>
2020-05-22 12:53:09 -0400

    Manejo de errores fuera de express
    rechazos no manipulados

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
+const app = require('./app')
 //console.log(process.env);
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
-);
+)
 
 mongoose
   .connect(DB, {
@@ -16,9 +16,18 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connected'));
+  .then(() => console.log('DB connected'))
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+const port = process.env.PORT || 3000
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message)
+  console.log('JUAN JANDLER REJECTION. Shutting down...')
+  // server.close() le da tiempo a los procesos que estan pendientes antes de..
+  server.close(() => {
+    console.log(process.exit(1))
+  })
+})
