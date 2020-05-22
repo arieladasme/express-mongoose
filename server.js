1e1b14c5072e2ea7873938089cd6cc2b67d917f1
Ariel <ariel.wm1@gmail.com>
2020-05-22 14:25:08 -0400

    Captura de excepciones no capturadas

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ dotenv.config({ path: './config.env' })
 const app = require('./app')
 //console.log(process.env);
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION. Shutting down...')
+  console.log(err.name, err.message)
+  // server.close() le da tiempo a los procesos que estan pendientes antes de..
+  console.log(process.exit(1))
+})
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -24,8 +31,8 @@ const server = app.listen(port, () => {
 })
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message)
   console.log('JUAN JANDLER REJECTION. Shutting down...')
+  console.log(err)
   // server.close() le da tiempo a los procesos que estan pendientes antes de..
   server.close(() => {
     console.log(process.exit(1))
