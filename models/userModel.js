866f086036827fa883db865bb51a1c60b5fcea3f
Ariel <ariel.wm1@gmail.com>
2020-05-22 22:15:36 -0400

    Gestion de contraseñas
    bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 // name email photo password passwordConfim
 
@@ -25,7 +26,27 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Must confirm email'],
+    validate: {
+      // This only works on CREATE and SAVE !
+      validator: function (el) {
+        return el === this.password
+      },
+      message: 'Password are not the same!',
+    },
   },
+})
+
+// Encriptar pw
+userSchema.pre('save', async function (next) {
+  //
+  // si la pw ha sido modificada se ejecutara esta f()
+  if (!this.isModified('password')) return next()
+
+  // hash(encripta) pw whit cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+  // delete pwConfirm
+  this.passwordConfirm = undefined
+  next()
 })
 
 const User = mongoose.model('User', userSchema)
