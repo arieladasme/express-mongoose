6794b9ce4388d06a5393fe7989155be10c95292d
Ariel <ariel.wm1@gmail.com>
2020-05-26 17:22:20 -0400

    Preventing Parameter Pollution
    install hpp

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
+const hpp = require('hpp')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const AppError = require('./utils/appError')
@@ -40,6 +41,20 @@ app.use(mongoSanitize())
 
 // Data sanitization XSS - inyeccion de codigo html
 app.use(xss())
+
+// Prevent parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+)
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`))
