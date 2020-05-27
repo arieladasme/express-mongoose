39f90184baab003b06a20fdbe3b10e90fb20cc25
Ariel <ariel.wm1@gmail.com>
2020-05-27 15:03:29 -0400

    Modelado datos geoespaciales

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
-const fs = require('fs');
-const dotenv = require('dotenv');
-const Tour = require('./../../models/tourModel');
+const mongoose = require('mongoose')
+const fs = require('fs')
+const dotenv = require('dotenv')
+const Tour = require('./../../models/tourModel')
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config.env' })
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
 mongoose
   .connect(DB, {
@@ -17,15 +14,13 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connected'));
+  .then(() => console.log('DB connected'))
 
 /**
  *  READ JSON FILE
  */
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 /**
  *  IMPORT DATA INTO DB
@@ -33,13 +28,13 @@ const tours = JSON.parse(
 
 const importData = async () => {
   try {
-    await Tour.create(tours);
-    console.log('Data successfully loaded!');
+    await Tour.create(tours)
+    console.log('Data successfully loaded!')
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-  process.exit();
-};
+  process.exit()
+}
 
 /**
  *  DELETE ALL DATA FROM COLLECTION
@@ -47,16 +42,16 @@ const importData = async () => {
 
 const deleteData = async () => {
   try {
-    await Tour.deleteMany();
-    console.log('Data successfully deleted!');
+    await Tour.deleteMany()
+    console.log('Data successfully deleted!')
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-  process.exit();
-};
+  process.exit()
+}
 
 if (process.argv[2] === '--import') {
-  importData();
+  importData()
 } else if (process.argv[2] === '--delete') {
-  deleteData();
+  deleteData()
 }
