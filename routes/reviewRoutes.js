ef55ce6199e1e9c81c6fa622a4b07fd326fc4119
Ariel <ariel.wm1@gmail.com>
2020-05-30 15:29:36 -0400

    Factory function
    Update & Create

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { protect, restrictTo } = require('../controllers/authController')
 
-const { createReview, getAllReviews, deleteReview } = require('./../controllers/reviewController')
+const {
+  createReview,
+  getAllReviews,
+  deleteReview,
+  updateReview,
+  setTourUserIds,
+} = require('./../controllers/reviewController')
 
 const router = express.Router({ mergeParams: true })
 
-router.route('/').post(protect, restrictTo('user'), createReview).get(getAllReviews)
-router.route('/:id').delete(deleteReview)
+router.route('/').post(protect, restrictTo('user'), setTourUserIds, createReview).get(getAllReviews)
+router.route('/:id').patch(updateReview).delete(deleteReview)
 
 module.exports = router
